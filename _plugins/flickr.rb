71dab81a391f9afc471576bb25030d7f63650f02
Jonas Forsberg <himynameisjonas@gmail.com>
2012-01-19 20:42:43 +0100

    cache flickr requests for faster rendering

--- _plugins/flickr.rb
+++ _plugins/flickr.rb
@@ -1,28 +1,13 @@
-#require 'fleakr'
-#module Jekyll
-  #class FlickrImage < Liquid::Tag
-    #    #def initialize(tag_name, text, tokens)
-      #super
-      #@image = text #    #end
-
-    #def render(context)
-      #"<img src='#{@image}' />"
-    #end
-  #end
-#end
-
-#Liquid::Template.register_tag('flickr_image', Jekyll::FlickrImage)
-
 require 'liquid'
 require 'fleakr'
-Fleakr.api_key = "c794c3f00be2130085e3cdef2f06aeb6"
 
-# Percent encoding for URI conforming to RFC 3986.
-# Ref: http://tools.ietf.org/html/rfc3986#page-12
+Fleakr.api_key = "c794c3f00be2130085e3cdef2f06aeb6"
+CACHED_IMAGES = {}
+
 module Flickr
   def flickr_image(url)
-    image = Fleakr.resource_from_url(url)
-    return "<img class='flickr-image align-center' alt='#{image.title}' src='#{image.large.url}'>"
+    CACHED_IMAGES[url] ||= Fleakr.resource_from_url(url)
+    return "<img class='flickr-image align-center' alt='#{CACHED_IMAGES[url].title}' src='#{CACHED_IMAGES[url].large.url}'>"
   end
 end
 
