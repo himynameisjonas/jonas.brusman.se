755cfd219e3f0cab2011b3b772bb5be69010c661
Jonas Forsberg <himynameisjonas@gmail.com>
2012-01-21 00:20:08 +0100

    added tags + medium flickr image to rss-feed

--- _plugins/flickr.rb
+++ _plugins/flickr.rb
@@ -6,8 +6,17 @@ CACHED_IMAGES = {}
 
 module Flickr
   def flickr_image(url)
+    return "<img class='flickr-image align-center' alt='#{image_object(url).title}' src='#{image_object(url).large.url}'>"
+  end
+
+  def flickr_medium_image(url)
+    return "<img class='flickr-image align-center' alt='#{image_object(url).title}' src='#{image_object(url).medium.url}'>"
+  end
+
+  private
+
+  def image_object(url)
     CACHED_IMAGES[url] ||= Fleakr.resource_from_url(url)
-    return "<img class='flickr-image align-center' alt='#{CACHED_IMAGES[url].title}' src='#{CACHED_IMAGES[url].large.url}'>"
   end
 end
 
