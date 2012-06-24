33d53903d202d27e6013615060bb68ea0329e930
Jonas Forsberg <himynameisjonas@gmail.com>
2012-06-24 15:29:07 +0200

    responsive images

--- _plugins/flickr.rb
+++ _plugins/flickr.rb
@@ -9,7 +9,23 @@ CACHED_IMAGES = {}
 
 module Flickr
   def flickr_image(url)
-    return "<img class='flickr-image align-center' alt='#{image_object(url).title}' src='#{image_object(url).large.url}'>"
+    image = image_object(url)
+    sizes = {}
+    sizes[nil] = image.medium.url unless image.medium.nil?
+    sizes[320] = image.small_320.url unless image.small_320.nil?
+    sizes[500] = image.medium.url unless image.medium.nil?
+    sizes[640] = image.medium_640.url unless image.medium_640.nil?
+    sizes[800] = image.medium_800.url unless image.medium_800.nil?
+    sizes[1024] = image.large.url unless image.large.nil?
+    sizes[1600] = image.large_1600.url unless image.large_1600.nil?
+    sizes[2048] = image.large_2048.url unless image.large_2048.nil?
+
+    "<figure class='flickr-image align-center' #{sizes.map{|s| "data-media#{s.first}='#{s.last}'"}.join(" ")} alt='#{image.title}' title='#{image.title}'>
+    <noscript>
+      <img class='flickr-image align-center' alt='#{image.title}' src='#{image.large.url}'>
+    </noscript>
+    </figure>
+    "
   end
 
   def flickr_medium_image(url)
