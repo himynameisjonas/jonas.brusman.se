f86ac6554c7b512755039c6eb95ba08e7c588ba9
Jonas Forsberg <himynameisjonas@gmail.com>
2013-06-12 11:24:19 +0200

    Moar responsive images

--- _plugins/flickr.rb
+++ _plugins/flickr.rb
@@ -46,14 +46,14 @@ module Flickr
     CACHE.fetch(url) do
       fleakr_image = Fleakr.resource_from_url(url)
       image = {:sizes => {}}
-      image[:sizes][nil] = fleakr_image.medium.url unless fleakr_image.medium.nil?
-      image[:sizes][320] = fleakr_image.small_320.url unless fleakr_image.small_320.nil?
-      image[:sizes][500] = fleakr_image.medium.url unless fleakr_image.medium.nil?
-      image[:sizes][640] = fleakr_image.medium_640.url unless fleakr_image.medium_640.nil?
-      image[:sizes][800] = fleakr_image.medium_800.url unless fleakr_image.medium_800.nil?
-      image[:sizes][1024] = fleakr_image.large.url unless fleakr_image.large.nil?
-      image[:sizes][1600] = fleakr_image.large_1600.url unless fleakr_image.large_1600.nil?
-      image[:sizes][2048] = fleakr_image.large_2048.url unless fleakr_image.large_2048.nil?
+      image[:sizes][fleakr_image.medium.width.to_i] = fleakr_image.medium.url unless fleakr_image.medium.nil?
+      image[:sizes][fleakr_image.small_320.width.to_i] = fleakr_image.small_320.url unless fleakr_image.small_320.nil?
+      image[:sizes][fleakr_image.medium.width.to_i] = fleakr_image.medium.url unless fleakr_image.medium.nil?
+      image[:sizes][fleakr_image.medium_640.width.to_i] = fleakr_image.medium_640.url unless fleakr_image.medium_640.nil?
+      image[:sizes][fleakr_image.medium_800.width.to_i] = fleakr_image.medium_800.url unless fleakr_image.medium_800.nil?
+      image[:sizes][fleakr_image.large.width.to_i] = fleakr_image.large.url unless fleakr_image.large.nil?
+      image[:sizes][fleakr_image.large_1600.width.to_i] = fleakr_image.large_1600.url unless fleakr_image.large_1600.nil?
+      image[:sizes][fleakr_image.large_2048.width.to_i] = fleakr_image.large_2048.url unless fleakr_image.large_2048.nil?
       image[:title] = fleakr_image.title
       CACHE.set(url, image)
       image
