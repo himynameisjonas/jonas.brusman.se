33b53cde094a697b3d8fd7aad4238006b452351d
Jonas Brusman <himynameisjonas@gmail.com>
2019-02-02 15:43:58 +0100

    fix name in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Jonas Brusman",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
+      "I like to do high fives, take photographs and make cool things with Ruby and JavaScript."
   },
   plugins: [
     "gatsby-plugin-netlify-cache",
