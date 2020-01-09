fd325d5dc47deef8b34f76313cb3b4e7c3199a96
Jonas Brusman <himynameisjonas@gmail.com>
2020-01-09 17:13:37 +0100

    goatcounter

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
 require("prismjs/themes/prism-solarizedlight.css");
+
+exports.onRouteUpdate = ({ location }) => {
+  window.goatcounter &&
+    window.goatcounter.count &&
+    window.goatcounter.count({
+      page: location.pathname + location.search + location.hash
+    });
+};
