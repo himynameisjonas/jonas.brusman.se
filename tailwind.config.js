e12487bffa5226bf77b9645dd90d9580ad3a5198
Jonas Brusman <himynameisjonas@gmail.com>
2020-08-30 11:04:48 +0200

    more styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
   purge: ["./src/**/*.md", "./src/**/*.liquid", "./src/**/*.html"],
   theme: {
     extend: {
+      colors: {
+      'cabaret': {
+        50: '#FEF6F8',
+        100: '#FDEDF1',
+        200: '#FBD1DB',
+        300: '#F9B5C5',
+        400: '#F47E9A',
+        500: '#EF476F',
+        600: '#D74064',
+        700: '#8F2B43',
+        800: '#6C2032',
+        900: '#481521',
+        },
+      },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
