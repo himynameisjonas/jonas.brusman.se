3157d8c834f4d7dc48fbb4a41c33997a3c93b910
Jonas Brusman <jonas@brusman.se>
2021-10-27 15:13:34 +0200

    fix "undefined" in dev

--- src/11ty/tracking_script.js
+++ src/11ty/tracking_script.js
@@ -1,5 +1,7 @@
 module.exports = function () {
   if(process.env.NETLIFY) {
     return '<script data-goatcounter="https://brusman_se.goatcounter.com/count" async src="/js/count.js"></script>'
+  } else {
+    return '';
   }
 };
