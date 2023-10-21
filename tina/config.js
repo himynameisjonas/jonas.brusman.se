a6fe38be7f664ba958dc9363876f383c8ae841ee
Jonas Brusman <jonas@brusman.se>
2023-10-21 09:05:53 +0200

    Tina CMS

---
+++ tina/config.js
@@ -0,0 +1,69 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "src/site/misc",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "src/site",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/site/blog_posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            label: 'Photos',
+            name: 'photos',
+            type: 'string',
+            list: true,
+          },
+          {
+            label: 'Tags',
+            name: 'tags',
+            type: 'string',
+            list: true,
+          },
+          {
+            label: 'Syndications',
+            name: 'syndications',
+            type: 'string',
+            list: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
