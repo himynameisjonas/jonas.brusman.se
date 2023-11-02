1d03027e252f03ec7eb1d1527f263282f34ef2b4
Jonas Brusman <jonas@brusman.se>
2023-11-02 13:41:25 +0100

    cleanup image_url

--- src/11ty/image_url.js
+++ src/11ty/image_url.js
@@ -1,25 +1,14 @@
 const Image = require("@11ty/eleventy-img");
 
-
-const url = async function (path, {width}) {
-
-  let stats = await Image(path, {
-    widths: [width],
+const shortcode = async function (imagePath, sharing) {
+  let stats = await Image(imagePath, {
+    widths: [1200],
   });
 
   return stats.jpeg[0].url;
-}
-
-const shortcode = async function (imagePath, sharing) {
-    return url(imagePath,
-      {
-        width: 1200,
-      },
-    );
 
 }
 
 module.exports = {
-  url,
   shortcode
 }
