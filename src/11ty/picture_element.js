762005aa6b936864aeb0487aa1f6a98580b0b93b
Jonas Brusman <jonas@brusman.se>
2023-11-05 19:00:49 +0100

    New smaller responsive image

--- src/11ty/picture_element.js
+++ src/11ty/picture_element.js
@@ -1,7 +1,7 @@
 const Image = require("@11ty/eleventy-img");
 
 module.exports = async function (imagePath, alt, imgClass, sizes = "(max-width: 1000px) 100vw, 133vh") {
-  const widths = [1200, 2000, 3000, 4000];
+  const widths = [800, 1200, 2000, 3000, 4000];
   let stats = await Image(imagePath, {
     widths
   });
