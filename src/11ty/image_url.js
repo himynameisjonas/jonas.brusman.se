651af8c1d5f7a6a2fbbff07021a88dc1713a2ca2
Jonas Brusman <jonas@brusman.se>
2023-11-14 20:53:11 +0100

    Fix missing cache option

--- src/11ty/image_url.js
+++ src/11ty/image_url.js
@@ -3,6 +3,9 @@ const Image = require("@11ty/eleventy-img");
 const shortcode = async function (imagePath, sharing) {
   let stats = await Image(imagePath, {
     widths: [1200],
+    cacheOptions: {
+      duration: "10y",
+    }
   });
 
   return stats.jpeg[0].url;
