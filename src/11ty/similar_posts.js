e216dcca9e15d483b8a3f47ecf503412261ac690
Jonas Brusman <jonas@brusman.se>
2023-12-27 21:45:16 +0100

    Prioritize newer related posts

--- src/11ty/similar_posts.js
+++ src/11ty/similar_posts.js
@@ -7,6 +7,10 @@ module.exports = function(collection, path, tags){
   return collection.filter((post) => {
     return getSimilarTags(post.data.tags, tags) >= 2 && post.data.page.inputPath !== path;
   }).sort((a,b) => {
-    return getSimilarTags(b.data.tags, tags) - getSimilarTags(a.data.tags, tags);
+    const similarTagsDiff = getSimilarTags(b.data.tags, tags) - getSimilarTags(a.data.tags, tags);
+    if (similarTagsDiff === 0) {
+      return new Date(b.data.date) - new Date(a.data.date);
+    }
+    return similarTagsDiff;
   });
 };
