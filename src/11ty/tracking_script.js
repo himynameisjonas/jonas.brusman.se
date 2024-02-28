a46c98d0119be94c643737c40352d40280687151
Jonas Brusman <jonas@brusman.se>
2024-02-28 09:53:10 +0100

    Remove tinylytics test

--- src/11ty/tracking_script.js
+++ src/11ty/tracking_script.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   if (process.env.NETLIFY) {
-    return '<script data-goatcounter="https://brusman_se.goatcounter.com/count" async src="/js/count.js"></script><script src="https://tinylytics.app/embed/ojQJg4KosvEzB9jez3Dv.js" defer></script>';
+    return '<script data-goatcounter="https://brusman_se.goatcounter.com/count" async src="/js/count.js"></script>';
   } else {
     return "";
   }
