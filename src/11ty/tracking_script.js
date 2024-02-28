e1754735a5ce4bf3b1e40f3365d182c880411a1d
Jonas Brusman <jonas@brusman.se>
2024-02-28 17:21:16 +0100

    Enable analytics on Cloudflare

--- src/11ty/tracking_script.js
+++ src/11ty/tracking_script.js
@@ -1,5 +1,5 @@
 module.exports = function () {
-  if (process.env.NETLIFY) {
+  if (process.env.ENABLE_ANALYTICS) {
     return '<script data-goatcounter="https://brusman_se.goatcounter.com/count" async src="/js/count.js"></script>';
   } else {
     return "";
