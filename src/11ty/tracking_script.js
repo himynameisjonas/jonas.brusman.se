22427ea97beae7e89f68d25a9c006d715cf63fa8
Jonas Brusman <jonas@brusman.se>
2024-02-28 17:36:06 +0100

    typo

--- src/11ty/tracking_script.js
+++ src/11ty/tracking_script.js
@@ -1,5 +1,5 @@
 module.exports = function () {
-  if (process.env.ENABLE_ANALYTICS) {
+  if (process.env.ENABLE_ANALYTICS === "true") {
     return '<script data-goatcounter="https://brusman_se.goatcounter.com/count" async src="/js/count.js"></script>';
   } else {
     return "";
