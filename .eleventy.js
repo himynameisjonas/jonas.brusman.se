e37bc2159e3f584184cdca408ba3a77e56549798
Jonas Brusman <jonas@brusman.se>
2024-03-25 14:37:48 +0100

    Simplify setup with 11ty-image

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,33 @@
 const extractExcerpt = require("./src/11ty/extract_excerpt");
 const imageUrl = require("./src/11ty/image_url");
 const minifyHtml = require("./src/11ty/minify_html");
-const pictureElement = require("./src/11ty/picture_element");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const similarPosts = require("./src/11ty/similar_posts");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const trackingScript = require("./src/11ty/tracking_script");
 const typeCollection = require("./src/11ty/type_collection");
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(syntaxHighlight);
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		extensions: "html",
+
+    cacheOptions: {
+      duration: "10y",
+    },
+		formats: ["webp", "jpeg"],
+
+		widths: ["auto"],
+
+    urlPath: "/img/",
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async"
+		}
+	});
+
 
   eleventyConfig.setDataDeepMerge(true);
   eleventyConfig.setUseGitIgnore(false);
@@ -30,7 +47,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addShortcode("excerpt", extractExcerpt);
   eleventyConfig.addShortcode("image_url", imageUrl.shortcode);
-  eleventyConfig.addShortcode("picture_element", pictureElement);
   eleventyConfig.addShortcode("tracking_script", trackingScript);
 
   return {
