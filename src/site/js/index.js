fb03104711573f02c5902dc62afdf437810a5eb7
Jonas Brusman <jonas@brusman.se>
2024-04-14 08:42:01 +0200

    Swup + analytics

--- src/site/js/index.js
+++ src/site/js/index.js
@@ -2,6 +2,14 @@ import Swup from "swup";
 import SwupPreloadPlugin from "@swup/preload-plugin";
 import "./open_heart_element";
 
-new Swup({
+const swup = new Swup({
   plugins: [new SwupPreloadPlugin()],
+});
+
+swup.hooks.on("page:view", () => {
+  if (!window.goatcounter) return;
+
+  window.goatcounter.count({
+    path: location.pathname + location.search + location.hash,
+  });
 });
