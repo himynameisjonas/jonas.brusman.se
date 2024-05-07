842d6de1b2529091a20f2f70c6519fef836fe709
Jonas Brusman <jonas@brusman.se>
2024-05-07 22:14:48 +0200

    Don't fail when testing local images

--- src/11ty/image_url.cjs
+++ src/11ty/image_url.cjs
@@ -1,15 +1,20 @@
 const Image = require("@11ty/eleventy-img");
 
 const shortcode = async function (imagePath, sharing) {
-  let stats = await Image(imagePath, {
-    formats: ["jpeg"],
-    widths: [1200],
-    cacheOptions: {
-      duration: "10y",
-    },
-  });
+  try {
+    let stats = await Image(imagePath, {
+      formats: ["jpeg"],
+      widths: [1200],
+      cacheOptions: {
+        duration: "10y",
+      },
+    });
 
-  return stats.jpeg[0].url;
+    return stats.jpeg[0].url;
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
 };
 
 module.exports = {
