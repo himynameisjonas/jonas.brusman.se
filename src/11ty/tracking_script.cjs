0f3837138abc8c394a9e45336c389c6f1f8d1b42
Jonas Brusman <jonas@brusman.se>
2025-08-12 22:04:23 +0200

    Umami

--- src/11ty/tracking_script.cjs
+++ src/11ty/tracking_script.cjs
@@ -1,6 +1,6 @@
 module.exports = function () {
   if (process.env.ENABLE_ANALYTICS === "true") {
-    return '<script data-goatcounter="https://brusman_se.goatcounter.com/count" async src="/js/count.js"></script>';
+    return '<script defer src="https://um.brusman.se/script.js" data-website-id="76f98f72-cc93-4165-8ed7-5217b2e023c5"></script><script data-goatcounter="https://brusman_se.goatcounter.com/count" async src="/js/count.js"></script>';
   } else {
     return "";
   }
